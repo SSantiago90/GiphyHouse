492dc1ab0692423a4ea74c14cb4999397912bc7f
ssantiago90 <salkin.santiago@gmail.com>
2022-10-26 18:44:51 -0300

    initial code

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import GifImg from "./GifImg";
 
 function App() {
   const [gifList, setGifList] = useState([]);
-  const [url, setUrl] = useState("");
 
-  const apikey = "inserta-tu-apikey";
-
-  function handleSearch( keyword){
-    setUrl(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}`)
-  }
+  const keyword = "dogs";
+  const apikey = "YdRG7nj8w8JdpJHzyy1NUMm8hEiHfqPR";
+  const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}`;
 
   useEffect(() => {
     fetch(url)
@@ -18,15 +15,15 @@ function App() {
       .then((json) => setGifList(json.data));
   }, [url]);
 
-
   return (
     <div className="App">
-      <button onClick={()=>handleSearch("dogs")}>Buscar Perritos</button>
-      <button onClick={()=>handleSearch("cats")}>Buscar Gatitos</button>
-      <button onClick={()=>handleSearch("pokemon")}>Buscar Pokemons</button>
       <h1>Gifs Coderhouse</h1>
       {gifList.map((item) => (
-        <GifImg key={item.id} title={item.title} url={item.images.original.url} />
+        <GifImg
+          key={item.id}
+          title={item.title}
+          url={item.images.original.url}
+        />
       ))}
     </div>
   );
