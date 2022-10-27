1dcf1bcb941296fb4942f186e1eee9503f84238a
ssantiago90 <salkin.santiago@gmail.com>
2022-10-26 22:45:13 -0300

    clase7-34785

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import GifImg from "./GifImg";
 function App() {
   const [gifList, setGifList] = useState([]);
 
-  const keyword = "dogs";
+  const keyword = "pokemon";
   const apikey = "YdRG7nj8w8JdpJHzyy1NUMm8hEiHfqPR";
   const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}`;
 
-  useEffect(() => {
-    fetch(url)
-      .then((respuesta) => respuesta.json())
-      .then((json) => setGifList(json.data));
-  }, [url]);
+  const apiFetch = async () => {
+    const response = await  fetch(url);
+    const json = await response.json();
+    setGifList(json.data)
+  }
+
+  useEffect( () => {
+    apiFetch();     
+  }, []);
 
   return (
     <div className="App">
