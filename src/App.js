0c7c1dc91e6e191014fc1b60cda75f661cab8193
ssantiago90 <salkin.santiago@gmail.com>
2022-11-06 18:33:49 -0300

    search gifs

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import GifImg from "./GifImg";
 
 function App() {
   const [gifList, setGifList] = useState([]);
+  const [keyword, setKeyword] = useState("dogs");
 
-  const keyword = "pokemon";
   const apikey = "YdRG7nj8w8JdpJHzyy1NUMm8hEiHfqPR";
 
-  const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}`;
-
   useEffect(() => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apikey}&limit=5&offset=100`;
     fetch(url)
       .then((respuestahttp) => respuestahttp.json())
       .then((jsonData) => {
         console.log(jsonData);
         setGifList(jsonData.data);
       });
-  }, []);
+  }, [keyword]);
+
+  function handleSearch(evt) {
+    setKeyword(evt.target.value);
+  }
 
   return (
     <div className="App">
+      <input onChange={handleSearch}></input>
+
       <h1>Gifs Coderhouse</h1>
       {gifList.map((gif) => (
         <GifImg key={gif.id} title={gif.title} url={gif.images.downsized.url} />
